0ed2a136271c13a8b14908b666809b0461dcfdd1
HeoGwangYoung <heogwangyoung@gmail.com>
2023-04-13 10:17:36 +0900

    새로운_리파지토리

---
+++ hello_01/src/com/callor/hello/HelloKorea.java
@@ -0,0 +1,11 @@
+package com.callor.hello;
+
+public class HelloKorea {
+	public static void main(String[] args) {
+		System.out.println(10 + 3);
+		System.out.println(10 - 3);
+		System.out.println(10 * 3);
+		System.out.println(10 / 3);
+		System.out.println(10 % 3);
+	}
+}
