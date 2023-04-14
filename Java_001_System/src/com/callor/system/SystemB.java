0b8cbcd3163dbd36e0c8dd1ec1b11c31d3bb3ffb
HeoGwangYoung <heogwangyoung@gmail.com>
2023-04-14 10:44:50 +0900

    System클래스

---
+++ Java_001_System/src/com/callor/system/SystemB.java
@@ -0,0 +1,14 @@
+package com.callor.system;
+
+public class SystemB {
+	
+	public static void main(String[] args) {
+		// form에 맞추도록 데이터를 출력하는 method
+		System.out.printf("%d + %d = %d\n",30 ,40 ,30 + 40); // printf => print format
+		System.out.println();
+		System.out.printf("%d x %d = %d\n",99 ,77 ,77 * 99);
+		System.out.println();
+		System.out.print("\n");
+		System.out.print("대한민국\n우리나라\nRepublic of Korea");
+	}
+}
