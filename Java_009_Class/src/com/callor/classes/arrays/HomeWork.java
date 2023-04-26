62ae6150e7ecc8918636eff4981434c6129e409a
HeoGwangYoung <heogwangyoung@gmail.com>
2023-04-26 16:35:04 +0900

    array

--- Java_009_Class/src/com/callor/classes/arrays/HomeWork.java
+++ Java_009_Class/src/com/callor/classes/arrays/HomeWork.java
@@ -6,15 +6,17 @@ import com.callor.classes.service.ScoreServiceA;
 public class HomeWork {
 
 	public static void main(String[] args) {
+		// 클래스 배열 생성
 		ScoreDto[] scores = new ScoreDto[10];
 
-		// 클래스 배열 생성
+		// 클래스 배열에 각각의 객체 생성
 		for (int i = 0; i < scores.length; i++) {
 			scores[i] = new ScoreDto();
 		}
 		// 이름과 학번을 담을 배열 선언
 		String[] name = {"홍길동", "이몽룡", "성춘향", "장보고", "임꺽정", "김철수", "김희경", "이기동", "박철수", "한동후"};
 		String[] num = {"0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010"};
+		// 총합을 저장할 변수 선언
 		int korTotal = 0;
 		int engTotal = 0;
 		int mathTotal = 0;
@@ -30,11 +32,12 @@ public class HomeWork {
 			engTotal += scores[i].scEng;
 			mathTotal += scores[i].scMath;
 		}
-		
+		// 타이틀 출력
 		System.out.println("=".repeat(70));
 		System.out.println("학번\t이름\t국어\t영어\t수학\t총점\t평균");
 		System.out.println("-".repeat(70));
 		
+		// 서비스 메서드를 이용하기 위해 객체 생성
 		ScoreServiceA scoreService = new ScoreServiceA();
 		
 		// 점수 출력
