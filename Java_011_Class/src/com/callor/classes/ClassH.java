3e40af3afbb2eba836ede14f721c5c360d4d4705
HeoGwangYoung <heogwangyoung@gmail.com>
2023-05-02 16:55:45 +0900

    homework

--- Java_011_Class/src/com/callor/classes/ClassH.java
+++ Java_011_Class/src/com/callor/classes/ClassH.java
@@ -9,19 +9,23 @@ public class ClassH {
 		
 		int num1;
 		int num2;
-		
 		while(true) {
-			System.out.println("두개의 정수를 입력 해주세요");
-			System.out.println("중단하려면 exit 입력");
+			System.out.println("두개의 정수를 입력 해주세요 종료하려면 -입력");
+			
 			System.out.print("정수 1 >> ");
-			num1 = scan.nextInt();
+			String str = scan.nextLine();
+			if (str.equals("-")) {
+				break;
+			}
+			num1 = Integer.parseInt(scan.nextLine());
 			System.out.print("정수 2 >> ");
 			num2 = scan.nextInt();
 			
-			System.out.printf("%d + %d = %d", num1, num2, num1 + num2);
-			System.out.printf("%d - %d = %d", num1, num2, num1 - num2);
-			System.out.printf("%d * %d = %d", num1, num2, num1 * num2);
-			System.out.printf("%d / %d = %f.2", num1, num2, (float)num1 / num2);
+			System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
+			System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
+			System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
+			System.out.printf("%d / %d = %f.2\n", num1, num2, (float)num1 / num2);
 		}
+		System.out.println("OVER");
 	}
 }
